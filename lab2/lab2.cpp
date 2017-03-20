2a6097b7fefb38cbcb63d1d5266f41ac159e5c99
VictorIstratii151 <victor.istratii@faf.utm.md>
2017-03-20 17:28:09 +0200

    created a button which will move at window resizing

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -1,23 +1,51 @@
 #include <windows.h>
 
-#define IDC_BUTTON 1
+#define IDC_BUTTON1 1
+
 
 const char g_szClassName[] = "myWindowClass";
+HWND Button1;
 
 // Step 4: the Window Procedure
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
 	PAINTSTRUCT ps;
 	RECT rcClient;
+	HDC hdc;
 
 	switch (msg)
 	{
+	case WM_CREATE:
+	{
+		hdc = BeginPaint(hwnd, &ps);
+		GetClientRect(hwnd, &rcClient);
+		int width = rcClient.right - rcClient.left;
+		int height = rcClient.bottom - rcClient.top;
+		Button1 = CreateWindowEx(NULL, "BUTTON", "Hi", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON, width / 4, height / 4, 70, 30, hwnd, (HMENU)IDC_BUTTON1, GetModuleHandle(NULL), NULL);
+		EndPaint(hwnd, &ps);
+	}
+	break;
+	
 	case WM_CLOSE:
 		DestroyWindow(hwnd);
 		break;
+
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		break;
+
+	case WM_PAINT:
+	{
+		hdc = BeginPaint(hwnd, &ps);
+		GetClientRect(hwnd, &rcClient);
+		int width = rcClient.right - rcClient.left;
+		int height = rcClient.bottom - rcClient.top;
+		MoveWindow(Button1, width / 4, height / 4, 70, 30, TRUE);
+		EndPaint(hwnd, &ps);
+
+	}
+	break;
+
 	default:
 		return DefWindowProc(hwnd, msg, wParam, lParam);
 	}
