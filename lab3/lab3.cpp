966502cc1171eb18ea3efc4c2fad6a06c8e25897
VictorIstratii151 <victor.istratii@faf.utm.md>
2017-04-06 21:05:15 +0300

    Added the possibility to draw 5 random lines from menu and to erase them from menu too

--- lab3/lab3.cpp
+++ lab3/lab3.cpp
@@ -3,9 +3,56 @@
 #include <time.h>
 #include "resource.h"
 
+void drawLines(HWND hwnd)
+{
+	srand(time(NULL));
+
+	PAINTSTRUCT ps;
+	RECT rcClient;
+	HPEN hPenOld;
+	HDC hdc; 
+
+	static int a, b, c;
+
+	hdc = BeginPaint(hwnd, &ps);
+
+	GetClientRect(hwnd, &rcClient);
+
+	int width = rcClient.right - rcClient.left;
+	int height = rcClient.bottom - rcClient.top;
+
+	for (int i = 0; i < 5; i++)
+	{
+		a = rand() % 255 + 1;
+		b = rand() % 255 + 1;
+		c = rand() % 255 + 1;
+
+		HPEN hLinePen;
+		COLORREF qRandomColor = RGB(a, b, c);
+		hLinePen = CreatePen(PS_SOLID, rand() % 20 + 1, qRandomColor);
+		hPenOld = (HPEN)SelectObject(hdc, hLinePen);
+
+		int xStart = rand() % width;
+		int yStart = rand() % height;
+		int xEnd = rand() % width;
+		int yEnd = rand() % height;
+
+
+
+		MoveToEx(hdc, xStart, yStart, NULL);
+		LineTo(hdc, xEnd, yEnd);
+
+		SelectObject(hdc, hPenOld);
+		DeleteObject(hLinePen);
+	}
+
+	EndPaint(hwnd, &ps);
+}
+
 const char g_szClassName[] = "myWindowClass";
 HWND Button1;
 bool LineDraw = false;
+POINT coordinates[5];
 
 // Step 4: the Window Procedure
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
@@ -47,7 +94,9 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 
 			MoveWindow(Button1, 3 * (width / 4), height / 4, 70, 30, TRUE);
 
-			if (LineDraw == true) {
+		
+
+			/*if (LineDraw == true) {
 				for (int i = 0; i < 5; i++)
 				{
 					a = rand() % 255 + 1;
@@ -59,13 +108,21 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 					hLinePen = CreatePen(PS_SOLID, rand() % 20 + 1, qRandomColor);
 					hPenOld = (HPEN)SelectObject(hdc, hLinePen);
 
-					MoveToEx(hdc, rand() % width, rand() % height, NULL);
-					LineTo(hdc, rand() % width, rand() % height);
+					int xStart = rand() % width;
+					int yStart = rand() % height;
+					int xEnd = rand() % width;
+					int yEnd = rand() % height;
+
+					
+
+					MoveToEx(hdc, xStart, yStart, NULL);
+					LineTo(hdc, xEnd, yEnd);
 
 					SelectObject(hdc, hPenOld);
 					DeleteObject(hLinePen);
 				}
-			}
+				LineDraw = false;
+			}*/
 
 			HPEN hEllipsePen;
 			COLORREF qEllipseColor = RGB(0, 0, 255);
@@ -80,22 +137,23 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 			EndPaint(hwnd, &ps);
 		}
 	break;
-
+	
 	case WM_COMMAND:
 		{
 			switch (LOWORD(wParam))
 			{
 				case ID_DRAW_ERASE:
 				{
-					LineDraw = false;
-					GetClientRect(hwnd, &rcClient);
-					RedrawWindow(hwnd, &rcClient, NULL, RDW_ERASE);
-				}
-				case ID_DRAW_LINE:
-				{
-					LineDraw = true;
 					GetClientRect(hwnd, &rcClient);
 					InvalidateRect(hwnd, NULL, TRUE);
+					//RedrawWindow(hwnd, &rcClient, NULL, RDW_ERASE);
+				}
+				break;
+
+				case ID_DRAW_LINE:
+				{
+					InvalidateRect(hwnd, NULL, TRUE);
+					drawLines(hwnd);
 				}
 				break;
 			}
@@ -121,6 +179,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 	}
 	return 0;
 }
+
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	LPSTR lpCmdLine, int nCmdShow)
