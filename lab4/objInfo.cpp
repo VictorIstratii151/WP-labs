8cf2442757efd70b35a12c071fc9bbbd8ed52779
VictorIstratii151 <victor.istratii@faf.utm.md>
2017-05-01 17:59:02 +0300

    Added the function which checks for collisions

--- lab4/objInfo.cpp
+++ lab4/objInfo.cpp
@@ -31,3 +31,34 @@ void MovingObject::move(HDC & hdc, RECT rcClient)
 	SelectObject(hdc, GetStockObject(WHITE_BRUSH));
 	DeleteObject(brush);
 }
+
+void MovingObject::collision(RECT rcClient)
+{
+	if (center.x + 25 > rcClient.right - 1)
+	{
+		idHitObj = 1001;
+		squareCheck = TRUE;
+		delta.x = -abs(delta.x);
+	}
+	if (center.x - 25 < rcClient.left + 1)
+	{
+		squareCheck = TRUE;
+		idHitObj = 1001;
+		delta.x = abs(delta.x);
+	}
+	if (center.y + 25 > rcClient.bottom - 1)
+	{
+		idHitObj = 1001;
+		squareCheck = FALSE;
+		acceleration += 1;
+		delta.y = -abs(delta.y);
+
+	}
+	if (center.y - 25 < rcClient.top + 10)
+	{
+		acceleration += 1;
+		squareCheck = FALSE;
+		idHitObj = 1001;
+		delta.y = abs(delta.y);
+	}
+}
